c1f54748ac88314325b03a3062579c122a1fe332
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-01-08 21:44:15 -0600

    fixed bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
-import About from "./components/About"
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
